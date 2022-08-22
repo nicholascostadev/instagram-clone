5422dc08df8a5b3434dc9adcc53995b7a02f8c5e
Nicholas Costa <nicholascostadev@gmail.com>
2022-08-22 09:58:36 -0300

    Add Two more comments for the Home page

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,14 +1,17 @@
 import { Header } from './Header'
+import { Stories } from './Stories'
 
 export const Feed = () => {
   return (
     <>
       <Header />
-      <div className="px-96">
-        <main>
-          <h1>Feed</h1>
-        </main>
-      </div>
+      <main className="px-96 bg-gray-100 h-screen grid grid-cols-2 pt-10">
+        <div>
+          <Stories />
+          {/* Content */}
+        </div>
+        <div>{/* Other things */}</div>
+      </main>
     </>
   )
 }
