06cded2dc8ad05390a025f9e31e8e88a1fda85df
Nicholas Costa <nicholascostadev@gmail.com>
2022-08-22 19:07:09 -0300

    Rearrange Feed files

---
+++ src/components/Feed/FeedPosts.tsx
@@ -0,0 +1,18 @@
+import { Post } from '../Post'
+
+export const FeedPosts = () => {
+  return (
+    <div className="mt-4 flex flex-col gap-3">
+      <Post
+        postImage=""
+        postOwner="nicholascostadev"
+        postOwnerImage="https://github.com/nicholascostadev.png"
+      />
+      <Post
+        postOwner="diego3g"
+        postImage="https://github.com/nicholascostadev.png"
+        postOwnerImage="https://github.com/diego3g.png"
+      />
+    </div>
+  )
+}
