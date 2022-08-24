cef670cf9727df1972e40d74eee8b44abaaef735
Nicholas Costa <nicholascostadev@gmail.com>
2022-08-24 01:55:28 -0300

    Add Feed following Suggestions

--- src/components/Feed/FeedSuggestion.tsx
+++ src/components/Feed/FeedSuggestion.tsx
@@ -1,11 +1,23 @@
 import Image from 'next/image'
 
-export const Suggestion = () => {
+interface SuggestionProps {
+  name: string
+  suggestion: string
+  suggestionAmount: number | null
+  image: string
+}
+
+export const FeedSuggestion = ({
+  name,
+  suggestion,
+  suggestionAmount,
+  image,
+}: SuggestionProps) => {
   return (
     <div className="flex justify-center items-center gap-2">
       <div className="border rounded-full flex justify-center items-center">
         <Image
-          src="https://github.com/nicholascostadev.png"
+          src={image || 'https://github.com/nicholascostadev.png'}
           alt=""
           width={30}
           height={30}
@@ -14,9 +26,10 @@ export const Suggestion = () => {
         />
       </div>
       <div>
-        <strong className="text-sm">nicholas_m_costa</strong>
+        <strong className="text-sm">{name || 'nicholas_m_costa'}</strong>
         <p className="text-xs text-gray-400">
-          Followed by mauricio_fcarv + 50 more
+          Followed by {suggestion || 'mauricio_fcarv'}{' '}
+          {suggestionAmount ? `+ ${suggestionAmount} more` : ''}
         </p>
       </div>
       <a href="#" className="ml-auto text-xs text-blue-500 font-bold">
--- src/components/Feed/FollowSugestions.tsx
+++ src/components/Feed/FollowSugestions.tsx
@@ -1,13 +1,13 @@
-import { Suggestion } from './Suggestion'
+import { FeedSuggestion } from './FeedSuggestion'
 
 export const FeedFollowSuggestions = () => {
   return (
     <div className="flex flex-col gap-2 mt-4">
-      <Suggestion />
-      <Suggestion />
-      <Suggestion />
-      <Suggestion />
-      <Suggestion />
+      <FeedSuggestion image="" name="" suggestion="" suggestionAmount={5} />
+      <FeedSuggestion image="" name="" suggestion="" suggestionAmount={null} />
+      <FeedSuggestion image="" name="" suggestion="" suggestionAmount={5} />
+      <FeedSuggestion image="" name="" suggestion="" suggestionAmount={5} />
+      <FeedSuggestion image="" name="" suggestion="" suggestionAmount={null} />
     </div>
   )
 }
