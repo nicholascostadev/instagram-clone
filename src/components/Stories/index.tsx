96421f28d3108220e0f14a2e5703bfa0fb363d4b
Nicholas Costa <nicholascostadev@gmail.com>
2022-08-23 23:30:37 -0300

    Remove unecessary class

--- src/components/Stories/index.tsx
+++ src/components/Stories/index.tsx
@@ -2,7 +2,7 @@ import { Storie } from './Storie'
 
 export const Stories = () => {
   return (
-    <div className="border pl-2 py-2 bg-white rounded-lg min-h-fit flex gap-4 overflow-x-scroll">
+    <div className="border pl-2 py-2 bg-white rounded-lg flex gap-4 overflow-x-scroll">
       <Storie />
       <Storie />
       <Storie />
