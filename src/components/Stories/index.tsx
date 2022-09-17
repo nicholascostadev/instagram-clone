fd7000100f6c67ab55a988c7d9bf05d2ab19246f
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-17 12:34:37 -0300

    💄 style: Remove scrollbar on stories

--- src/components/Stories/index.tsx
+++ src/components/Stories/index.tsx
@@ -1,8 +1,9 @@
+import { CaretRight } from 'phosphor-react'
 import { Storie } from './Storie'
 
 export const Stories = () => {
   return (
-    <div className="border pl-2 py-2 bg-white rounded-lg flex gap-4 overflow-x-scroll">
+    <div className="relative border pl-2 py-2 bg-white rounded-lg flex gap-4 overflow-x-scroll scrollbar-hide">
       <Storie />
       <Storie />
       <Storie />
@@ -14,6 +15,10 @@ export const Stories = () => {
       <Storie />
       <Storie />
       <Storie />
+      <CaretRight
+        size={24}
+        className="absolute right-2 top-[50%] -translate-y-[50%] text-black border rounded-full bg-white"
+      />
     </div>
   )
 }
