4a1cc81ad1628027de94763c1a967c1eba001e40
Nicholas Costa <nicholascostadev@gmail.com>
2022-10-06 11:02:13 -0300

    💡 Comment complex code

--- src/server/router/protected-user-router.ts
+++ src/server/router/protected-user-router.ts
@@ -10,33 +10,42 @@ export const protectedUserRouter = createProtectedRouter()
       newName: z.string(),
     }),
     async resolve({ ctx, input }) {
-      // TODO: Comment more
+      // Pick user data for checking his info
       const userHistory = await ctx.prisma.user.findUnique({
         where: {
           id: ctx.session?.user?.id,
         },
       })
 
+      // Variables to see if the user is updating their username, name or both
       const isUsernameChanging = input.newUsername !== userHistory?.username
       const isNameChanging = input.newName !== userHistory?.name
 
+      // Variables only for readability
       const lastUpdatedNameTime = userHistory?.updatedNameAt
       const lastUpdatedUsernameTime = userHistory?.updatedUsernameAt
       const fourteenDays = 1000 * 60 * 60 * 24 * 14
 
+      // New object so we can manipulate it and make a single query only
+      // to the database at the end of the function
       const newUserObject = {
         ...userHistory,
         description: input.description,
         website: input.website,
       }
 
-      type ErrorObject = {
+      // Since we can't throw an error, because we want to pass through every
+      // route here, we have to use a variable to record if there were any errors
+      // or not
+      type TErrorObject = {
         errorName?: string
         errorUsername?: string
       }
+      const errorObject: TErrorObject = {}
 
-      const errorObject: ErrorObject = {}
-
+      // We chain the ifs so we can make something if user is changing his
+      // username and also if user is changing his name, and at the end, query
+      // the database only once
       if (isUsernameChanging) {
         if (
           lastUpdatedUsernameTime &&
@@ -44,15 +53,19 @@ export const protectedUserRouter = createProtectedRouter()
             fourteenDays
         ) {
           if (userHistory.updatedUsernameTimes + 1 > 2) {
+            // If the user has updated their username more than 2 times in the
+            // last 14 days, we will throw an error
             errorObject.errorUsername =
               "You can't change your username more than 2 times in 14 days"
           } else {
+            // Set the new values to the object
             newUserObject.updatedUsernameTimes =
               userHistory.updatedUsernameTimes + 1
 
             newUserObject.username = input.newUsername
           }
         } else {
+          // Set the new values to the object
           newUserObject.updatedUsernameTimes = 1
           newUserObject.username = input.newUsername
           newUserObject.updatedUsernameAt = new Date()
@@ -65,19 +78,27 @@ export const protectedUserRouter = createProtectedRouter()
           lastUpdatedNameTime.getTime() - new Date().getTime() < fourteenDays
         ) {
           if (userHistory.updatedNameTimes + 1 > 2) {
+            // If the user has updated their name more than 2 times in the
+            // last 14 days, we will throw an error
             errorObject.errorName =
               "You can't change your name more than 2 times in 14 days"
           } else {
+            // Set the new values to the object
             newUserObject.updatedNameTimes = userHistory.updatedNameTimes + 1
             newUserObject.name = input.newName
           }
         } else {
+          // Set the new values to the object
           newUserObject.updatedNameTimes = 1
           newUserObject.name = input.newName
           newUserObject.updatedNameAt = new Date()
         }
       }
 
+      // Final query to the database, with all the results from all if
+      // statements, now we have everything that we need to update, as
+      // the number of times he updated his username and name + the new
+      // data
       return {
         data: await ctx.prisma.user.update({
           where: {
